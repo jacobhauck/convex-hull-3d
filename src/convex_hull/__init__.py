63776d16bf3e651cf3218f4ce7574cddcdccbdb9
Jacob Hauck <hauckjp@gmail.com>
2026-02-26 15:53:36 -0500

    Refactor __init__.py

--- src/convex_hull/__init__.py
+++ src/convex_hull/__init__.py
@@ -1,6 +1,7 @@
-from .base import (
-    IntegrationResult,
-    integrate_peak,
-    visualize_peak
+from .peak_integrator import (
+    PeakIntegrator,
+    IntegrationStatus,
+    IntegrationResult
 )
-from .peak_integrator import IntegrationStatus
+
+from .visualize import visualize_peak
